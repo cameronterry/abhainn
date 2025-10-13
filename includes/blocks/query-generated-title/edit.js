bf1278f0d45f708ede25847d24919f907e9eba4e
Cameron <cameronterry@protonmail.com>
2025-10-13 12:22:27 +0100

    Put together a stub block for Query Generated Title.

---
+++ includes/blocks/query-generated-title/edit.js
@@ -0,0 +1,27 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Edit component.
+ *
+ * {@link https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/}
+ *
+ * @param {object}   props                  The block props.
+ * @param {object}   props.attributes       Block attributes.
+ * @param {string}   props.attributes.title Custom title to be displayed.
+ * @param {string}   props.className        Class name for the block.
+ * @param {object}   props.context          Context provided by higher blocks.
+ * @param {Function} props.setAttributes    Sets the value for block attributes.
+ * @returns {Function} Render the edit screen
+ */
+const QueryGeneratedTitleEdit = (props) => {
+	const blockProps = useBlockProps();
+
+	return (
+		<div { ...blockProps }>
+		</div>
+	);
+};
+export default QueryGeneratedTitleEdit;
